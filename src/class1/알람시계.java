8a9bbbe6dcb7f935c0d5aa28a09facfb0edce31e
정석진 <jeongseogjin@jeongseogjins-MacBook-Air.local>
2024-09-18 21:56:05 +0900

    p2884

--- src/class1/알람시계.java
+++ src/class1/알람시계.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class 알람시계 {
+public class 알람시계1 {
     public static void main(String[] args) throws IOException {
 
 
@@ -35,6 +35,7 @@ public class 알람시계 {
 
 
         System.out.println(sb);
+
     }
 }
 

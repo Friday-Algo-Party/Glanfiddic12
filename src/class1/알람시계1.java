1168a77399fe4120f0df24fb02a24d756cd44bf4
정석진 <jeongseogjin@jeongseogjins-MacBook-Air.local>
2024-09-18 21:53:38 +0900

    dddd

---
+++ src/class1/알람시계1.java
@@ -0,0 +1,40 @@
+package class1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 알람시계 {
+    public static void main(String[] args) throws IOException {
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        StringBuilder sb = new StringBuilder();
+
+        int h = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        if(m < 45) {
+
+            if(h == 0){
+                h = 23;
+                sb.append(h).append(' ');
+            } else {
+                h--;
+                sb.append(h).append(' ');
+            }
+
+            sb.append(m = 60 - (45 - m));
+        }
+        else {
+
+            sb.append(h).append(' ').append(m - 45);
+        }
+
+
+        System.out.println(sb);
+    }
+}
+

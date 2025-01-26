ce8163e41aef58bf6a13dac6d164297f884893e6
Jeong Seok Jin <52066509+Jeongseokjin@users.noreply.github.com>
2025-01-26 22:15:45 +0900

    P3052

---
+++ src/class1/나머지.java
@@ -0,0 +1,19 @@
+package class1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+
+public class 나머지 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        HashSet<Integer> remainders = new HashSet<>();
+
+        for(int i = 0; i < 10; i++) {
+            remainders.add(Integer.parseInt(br.readLine()) % 42);
+        }
+
+        System.out.println(remainders.size());
+    }
+}

11c3022caf0ad2113228d8abead39eb0d583cd56
Jeong Seok Jin <52066509+Jeongseokjin@users.noreply.github.com>
2025-01-27 14:48:02 +0900

    P1392

---
+++ src/Class1/소수구하기.java
@@ -0,0 +1,39 @@
+package Class1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 소수구하기 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        boolean[] isPrime = new boolean[N + 1];
+        for (int i = 2; i <= N; i++) {
+            isPrime[i] = true;
+        }
+
+        for (int i = 2; i <= Math.sqrt(N); i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= N; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = M; i <= N; i++) {
+            if (isPrime[i]) {
+                sb.append(i).append('\n');
+            }
+        }
+
+        System.out.print(sb);
+    }
+}

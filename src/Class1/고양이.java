e1ccd7d5bbf5b49da416ad88853bce7f8b3d3880
Jeong Seok Jin <52066509+Jeongseokjin@users.noreply.github.com>
2025-01-31 17:14:09 +0900

    p10171

---
+++ src/Class1/고양이.java
@@ -0,0 +1,15 @@
+package Class1;
+
+import java.io.IOException;
+
+public class 고양이 {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println( "\\    /\\\n" +
+                " )  ( ')\n" +
+                "(  /  )\n" +
+                " \\(__)|");
+
+    }
+}
+
